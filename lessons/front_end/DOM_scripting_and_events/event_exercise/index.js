6481aa1a7d88af90180b0222d3dee248f806bf71
robbie <rob.643@hotmail.co.uk>
2025-07-18 07:24:09 +0100

    imporved styling

--- lessons/front_end/DOM_scripting_and_events/event_exercise/index.js
+++ lessons/front_end/DOM_scripting_and_events/event_exercise/index.js
@@ -6,19 +6,57 @@
 // increment and decrement counter
 
 
-
-
-// const counter = document.getElementById('counter');
-// const incrementButton = document.getElementById('incrementButton');
-// const decrementButton = document.getElementById('decrementButton');
-
-// incrementButton.addEventListener('click', function () {
-//     counter.innerText = Number(counter.innerText) + 1;
-// });
-// decrementButton.addEventListener('click', function () {
-//         counter.innerText = Number(counter.innerText) -1;
-// });
-
 //=====================Exercise 2 ============================
 //? * Attach event to document and check id of element that kicked off event
 //  * Use it to start and stop music?
+const audio = new Audio('https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3');
+
+const startButton = document.getElementById('startButton');
+const pauseButton = document.getElementById('pauseButton');
+const restartButton = document.getElementById('restartButton');
+
+// startButton.addEventListener('click', function () {
+//     audio.play();
+// });
+
+// pauseButton.addEventListener('click', function () {
+//     audio.pause();
+// });
+
+// restartButton.addEventListener('click', function () {
+//     audio.currentTime = 0;
+//     audio.play();
+// });
+
+startButton.addEventListener('click', function (e) {
+    audio.play();
+});
+
+pauseButton.addEventListener('click', function () {
+    audio.pause();
+});
+
+restartButton.addEventListener('click', function () {
+    audio.currentTime = 0;
+
+});
+
+
+
+
+
+
+// =========================Answers=========================
+
+//==============Answer 1==============
+
+const counter = document.getElementById('counter');
+const incrementButton = document.getElementById('incrementButton');
+const decrementButton = document.getElementById('decrementButton');
+
+incrementButton.addEventListener('click', function () {
+    counter.innerText = Number(counter.innerText) + 1;
+});
+decrementButton.addEventListener('click', function () {
+        counter.innerText = Number(counter.innerText) -1;
+});
--- lessons/front_end/DOM_scripting_and_events/lesson_plan.md
+++ lessons/front_end/DOM_scripting_and_events/lesson_plan.md
@@ -1,7 +1,7 @@
 
 1. Go through flexbox recap exercises from previous lesson.
-2. Go through exercise traversing_dom, go through first 3 then leave the rest for student in own time
+2. Go through exercise traversing_dom, go through first 4 then leave the rest for student in own time
 3. Go through to_do exercise, go through first 3 exercises, then leave the rest for the student in own time
 4. Go through event_exercise, go through first 3 exercises, then leave rest for student
-5. Should now have foundation for dark mode button for portfolio
+5. Should now have foundation for dark mode button for portfolio, start work on this
   
