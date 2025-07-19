3f11c666c498a551b94b1eab0bf03b81eeda1c0c
robbie <rob.643@hotmail.co.uk>
2025-07-19 14:57:26 +0100

    improve first lesson

--- lessons/back_end/php/lesson_plan.md
+++ lessons/back_end/php/lesson_plan.md
@@ -2,5 +2,6 @@
 
 1. Install PHP and test running a php file.
 2. Set up VScode with intelliphense.
-3. work through php basics, focussing on aspects that will be useful for Laravel i.e inheritance and traits.
-4. set up xdebug for debugging
+3. set up live share
+4. Work through php basics, focussing on aspects that will be useful for Laravel i.e inheritance and traits.
+5. Set up xdebug for debugging.
--- lessons/back_end/php/vanilla_php_basics.php
+++ lessons/back_end/php/vanilla_php_basics.php
@@ -4,26 +4,40 @@ declare(strict_types=1);
 // ===============================
 // PHP Basics for Laravel Lesson 1 
 // ===============================
+// PHP documentation: https://www.php.net/manual/en/index.php
 
 // ---------------------------------
 // 1. Variables and Types
 // ---------------------------------
+// var_dump useful
 
 // String variable
+// string functions: https://www.php.net/manual/en/ref.strings.php
 
 // Integer variable
+// Math functions: https://www.php.net/manual/en/ref.math.php
 
 // Float variable
 
 // Boolean variable
 
-// associative arrays
+// arrays
+// array functions: https://www.php.net/manual/en/book.array.php
 
 // indexing arrays
 
 // adding to array []
 
 // removing from array unset
+
+// associative arrays
+// example $a= ['a'=>1, 'b'=>2];
+
+// looping through associative arrays
+// example 
+// foreach ($myArray as $key => $value) {
+//     echo "$key: $value\n";
+// }
 
 
 // ---------------------------------
@@ -35,6 +49,17 @@ declare(strict_types=1);
 
 
 // Function with type declarations and return type
+
+// array_map https://www.php.net/manual/en/function.array-map.php
+// example 
+// $b = array_map(function ($v) {return $v + 2;}, $a);
+// array_filter https://www.php.net/manual/en/function.array-filter.php 
+// example
+// $b = array_filter($a, function($v) {return $v==2;});
+// array_reduce https://www.php.net/manual/en/function.array-reduce.php
+// example
+// $b = array_reduce($a, function($a, $b) {return $a + $b;}, 0);
+
 
 // ---------------------------------
 // 3. Control Flow (If, Else, Loops)
@@ -67,10 +92,11 @@ declare(strict_types=1);
 
 // write function animateAnimal()
 
+// try doing the same for a Venue interface and club and restaurant
+
 // set up xdebug need to do via php -S localhost:8000
 // set up .vscode/launch.json
 //xdebug_break();
-
 
 
 // exercises ==========
