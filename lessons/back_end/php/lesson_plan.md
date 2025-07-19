e8dd4ec4af80a608fba49ab4bf7915fde210d296
robbie <rob.643@hotmail.co.uk>
2025-07-19 14:01:54 +0100

    improved first lesson

---
+++ lessons/back_end/php/lesson_plan.md
@@ -0,0 +1,6 @@
+## Lesson Plan
+
+1. Install PHP and test running a php file.
+2. Set up VScode with intelliphense.
+3. work through php basics, focussing on aspects that will be useful for Laravel i.e inheritance and traits.
+4. set up xdebug for debugging
---
+++ lessons/back_end/php/vanilla_php_basics.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+// ===============================
+// PHP Basics for Laravel Lesson 1 
+// ===============================
+
+// ---------------------------------
+// 1. Variables and Types
+// ---------------------------------
+
+// String variable
+
+// Integer variable
+
+// Float variable
+
+// Boolean variable
+
+// associative arrays
+
+// indexing arrays
+
+// adding to array []
+
+// removing from array unset
+
+
+// ---------------------------------
+// 2. Functions
+// ---------------------------------
+
+// Simple function
+// function addTwoNumbers
+
+
+// Function with type declarations and return type
+
+// ---------------------------------
+// 3. Control Flow (If, Else, Loops)
+// ---------------------------------
+
+// If-elseif-else statement
+
+// ternary if else 
+
+// For loop
+
+// While 
+
+// Foreach loop (arrays)
+
+// ---------------------------------
+// 4. Classes (OOP Basics)
+// ---------------------------------
+
+// implemet Booking
+// implement Restaurant
+
+// ---------------------------------
+// 5. Inheritance and Interfaces
+// ---------------------------------
+
+// Define an interface Animal
+
+// Duck and Dog class implementing interface an dimplementing speak()
+
+// write function animateAnimal()
+
+// set up xdebug need to do via php -S localhost:8000
+// set up .vscode/launch.json
+//xdebug_break();
+
+
+
+// exercises ==========
+// create Logger interface
+
+// implement StandardOutputLogger
+
+
+// ---------------------------------
+// 6. Traits
+// ---------------------------------
+
+// ---------------------------------
+// 7. Using PHP to serve responses
+// ---------------------------------
+
+// $a = $_GET['booking_id'];
+
+// echo "<div>booking_id: $a</div>";
