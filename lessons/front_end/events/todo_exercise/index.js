3913ce7208ca4b3028be0e0fd1987eb982956fa1
robbie <rob.643@hotmail.co.uk>
2025-07-25 06:46:52 +0100

    massively improve to do list so now delete button works per item and strikethrough works

--- lessons/front_end/events/todo_exercise/index.js
+++ lessons/front_end/events/todo_exercise/index.js
@@ -5,14 +5,13 @@
 const addButton = document.getElementById('addTodoBtn');
 const inputElement = document.getElementById('todoInput');
 
-addButton.addEventListener('click', function() {
+// addButton.addEventListener('click', function() {
     
-    const newElement = document.createElement('li')
-    newElement.textContent = inputElement.value;
+//     const newElement = document.createElement('li')
+//     newElement.textContent = inputElement.value;
     
 
-
-})
+// })
 
 // Add an event listener to the button element
 // Get the input element
@@ -20,35 +19,51 @@ addButton.addEventListener('click', function() {
 // Create a new list element and set textContent to the input
 // Append child to list item
 
-// ================= Exercise 2: Add a Delete Button =================
-
-// Find delete button in DOM using getElementById
-// Add an event listener to the button element
-// Get the list elements
-// remove last element child
 
 // ================= Exercise 2: Add a ability to strike through when completed =================
 
 // add a strike
+// when adding item add event listener that listens for click
+// when clicked use e.target.classList.toggle('completed')
+
+// ================= Exercise 3: Add ability to delete specific items
+//
 
 
 // ================= Answers =================
 
 // Answer 1
-// document.getElementById('addTodoBtn').addEventListener('click', function() {
-//     const input = document.getElementById('todoInput');
-//     const value = input.value.trim();
-//     if (value) {
-//         const li = document.createElement('li');
-//         li.textContent = value;
-//         document.getElementById('itemList').appendChild(li);
-//         input.value = '';
-//     }
-// });
+document.getElementById('addTodoBtn').addEventListener('click', function() {
+    const input = document.getElementById('todoInput');
+    const value = input.value.trim();
+    if (value) {
+        let li = document.createElement('li');
+        li.classList.add('list-item')
+        attachHandleClick(li)
+        li.innerHTML = "<div class='item-container'><div class='item'></div><button class='deleteTodoBtn'>Delete</button></div>"
+        li.querySelector(".item").textContent = value;
+        document.getElementById('itemList').appendChild(li);
+        input.value = '';
+    }
+});
 
-// Answer 2
-// document.getElementById('deleteTodoBtn').addEventListener('click', function() {
-//     let list = document.getElementById('itemList');
-//     list.lastElementChild.remove();
-// });
 
+
+//answer 2
+
+function attachHandleClick(element) {
+    element.addEventListener('click', (e) => {
+        e.target.classList.toggle('completed')
+    })
+
+}
+
+// answer 3
+
+document.addEventListener('click', (event) => {
+    
+    if (event.target.classList.contains('deleteTodoBtn')) {
+        
+        event.target.closest(".item-container").remove();
+    }
+})
