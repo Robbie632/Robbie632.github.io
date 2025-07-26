a66cc293635361917c5d2440cb4ed3e9a9ad3106
robbie <rob.643@hotmail.co.uk>
2025-07-26 07:14:03 +0100

    utilise document listeners rather than element listeners for to do list

--- lessons/front_end/events/todo_exercise/index.js
+++ lessons/front_end/events/todo_exercise/index.js
@@ -1,58 +1,52 @@
 // ================= Exercise 1: Add an "Add" Button =================
 
-// Find add button in DOM using getElementById
-
-const addButton = document.getElementById('addTodoBtn');
-const inputElement = document.getElementById('todoInput');
-
-// addButton.addEventListener('click', function() {
-    
-//     const newElement = document.createElement('li')
-//     newElement.textContent = inputElement.value;
-    
-
-// })
-
-// Add an event listener to the button element
-// Get the input element
-// Get the list element
-// Create a new list element and set textContent to the input
-// Append child to list item
-
-
 // ================= Exercise 2: Add a ability to strike through when completed =================
-
 
 // ================= Exercise 3: Add ability to delete specific items
 
-
+//================== Exercise 4: Add ability to edit individual items 
 
 // ================= Answers =================
 
 // Answer 1
-document.getElementById('addTodoBtn').addEventListener('click', function() {
-    const input = document.getElementById('todoInput');
-    const value = input.value.trim();
+document.addEventListener("click", function (e) {
+  
+    if (e.target.id == "todo") {
+    
+    const inputElement = document.getElementById("todoInput");
+    const value = inputElement.value.trim();
+
     if (value) {
-        let li = document.createElement('li');
-        li.classList.add('list-item')
-        li.innerHTML = "<div class='item-container'><div class='item'></div><button class='deleteTodoBtn'>Delete</button></div>"
-        li.querySelector(".item").textContent = value;
-        document.getElementById('itemList').appendChild(li);
-        input.value = '';
+      let li = document.createElement("li");
+      li.classList.add("list-item");
+      li.innerHTML =
+        "<div class='item-container'><div class='item'></div><button class='delete'>Delete</button></div>";
+      li.querySelector(".item").textContent = value;
+      document.getElementById("itemList").appendChild(li);
+      inputElement.value = "";
     }
+    inputElement.focus();
+  }
 });
 
 //answer 2
-document.addEventListener('click', (e) => {
-    if (e.target.classList.contains('item')) {
-        e.target.classList.toggle('completed')
-    }
-})
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("item")) {
+    e.target.classList.toggle("completed");
+  }
+});
 
 // answer 3
-document.addEventListener('click', (event) => {
-    if (event.target.classList.contains('deleteTodoBtn')) {  
-        event.target.closest("li").remove();
-    }
-})
+document.addEventListener("click", (event) => {
+  if (event.target.classList.contains("delete")) {
+    event.target.closest("li").remove();
+  }
+});
+
+// answer 4
+
+document.addEventListener("click", (event) => {
+  if (event.target.classList.contains("edit")) {
+    alert('edit clicked')
+  }
+});
