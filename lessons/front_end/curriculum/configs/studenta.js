2807ece36bd34972033bc577d023d3b417c5740f
robbie <rob.643@hotmail.co.uk>
2025-07-27 14:00:12 +0100

    improve and add to curriculum and put modules in config js file

---
+++ lessons/front_end/curriculum/configs/studenta.js
@@ -0,0 +1,136 @@
+const modules = [
+    {
+    name: "Portfolio",
+    topics: [
+      "Your portfolio will be the platform by which you can show all your projects",
+      "It shall be deployed using Github actions"
+    ],
+    progress: 30
+  },
+  {
+    name: "Setup",
+    topics: [
+      "Using VScode",
+      "Emmet abbreviatons",
+      "Prettier formatter",
+      "Install Live Server extension",
+      "Install Live Share extension",
+      "Introduction to developer tools"
+    ],
+    progress: 95
+  },
+  {
+    name: "HTML",
+    topics: [
+      "Semantic elements- h1, h2, div, p, section, input, ul, ol",
+      "Document object model (DOM)"
+    ],
+    progress: 20
+  },
+  {
+    name: "Styling",
+    topics: [
+      "Loading stylesheets",
+      "CSS selectors",
+      "styling basics - colors, borders etc,",
+      "Flexbox",
+      "CSS variables",
+      "Responsive design",
+    ],
+    progress: 20
+  },
+  {
+    name: "JavaScript Syntax",
+    topics: [
+      "Variables & data types",
+      "Functions",
+      "Arrays",
+      "Control Flow",
+      "Control structures",
+    ],
+    progress:0
+  },
+  {
+    name: "DOM Scripting",
+    topics: [
+      "Selecting elements",
+      "Manipulating DOM nodes",
+      "Creating and removing elements",
+      "Traversing the DOM",
+    ],
+    progress:90
+  },
+  {
+    name: "Events",
+    topics: [
+      "Running JavaScript alongside HTML",
+      "Event listeners",
+      "Event propagation",
+      "Event delegation",
+      "Custom events",
+      "Preventing default actions",
+    ],
+     progress:70
+  },
+  {
+    name:"Client Side Storage",
+    topics: [
+      "Local Storage",
+      "Cookies"
+    ],
+    progress:0
+  },
+  {
+    name: "NPM and project scaffolding",
+    topics: [
+      "starting projects",
+      "Installing and using packages",
+    ],
+    progress:0
+  },
+  {
+    name: "Server Client Architecture",
+    topics: [
+      "Client-server model",
+      "HTTP basics",
+      "Request/response cycle",
+    ],
+    progress:10
+  },
+    {
+    name: "JavaScript Syntax - Advanced",
+    topics: [
+      "Array functions",
+      "Objects",
+      "arrow notation",
+      "callbacks",
+      "destructuring",
+      "async"
+    ],
+    progress:0
+  },
+  {
+    name: "API Calls",
+    topics: [
+      "AJAX & Fetch API",
+      "Promises & async/await",
+      "RESTful APIs",
+      "Error handling",
+      "Parsing JSON",
+    ],
+    progress:0
+  },
+  {
+    name: "React Framework",
+    topics: [
+      "project setup",
+      "rendering JSX",
+      "props",
+      "states",
+      "effects"
+    ],
+    progress:0
+  },
+];
+
+export default modules;
--- lessons/front_end/curriculum/index.js
+++ lessons/front_end/curriculum/index.js
@@ -1,106 +1,10 @@
-const modules = [
-    {
-    name: "Portfolio",
-    topics: [
-      "Show examples of all other modules",
-      "Pogress corresponds to proportion of progress of all modules "
 
-    ],
-    progress: 0
-  },
-  {
-    name: "Setup",
-    topics: [
-      "Using VScode",
-      "Emmet abbreviatons",
-      "Prettier formatter",
-      "Live Server",
-      "Live Share",
-      "introduction to developer tools"
-    ],
-    progress: 95
-  },
-  {
-    name: "HTML",
-    topics: [
-      "Semantic elements",
-      "Nodes",
-      "Document object model"
-    ],
-    progress: 20
-  },
-  {
-    name: "Styling",
-    topics: [
-      "CSS basics",
-      "Flexbox",
-      "CSS variables",
-      "Responsive design",
-    ],
-    progress: 20
-  },
-  {
-    name: "DOM Scripting",
-    topics: [
-      "Selecting elements",
-      "Manipulating DOM nodes",
-      "Creating and removing elements",
-      "Traversing the DOM",
-    ],
-    progress:0
-  },
-  {
-    name: "Events",
-    topics: [
-      "Event listeners",
-      "Event propagation",
-      "Event delegation",
-      "Custom events",
-      "Preventing default actions",
-    ],
-     progress:0
-  },
-  {
-    name: "Server Client Architecture",
-    topics: [
-      "Client-server model",
-      "HTTP basics",
-      "Request/response cycle",
-    ],
-    progress:15
-  },
-  
-  {
-    name: "API Calls",
-    topics: [
-      "AJAX & Fetch API",
-      "Promises & async/await",
-      "RESTful APIs",
-      "Error handling",
-      "Parsing JSON",
-    ],
-    progress:0
-  },
-  {
-    name: "JavaScript Syntax",
-    topics: [
-      "Variables & data types",
-      "Functions & scope",
-      "ES6+ features",
-      "Control structures",
-      "Error handling",
-    ],
-    progress:0
-  },
-  {
-    name: "React Framework",
-    topics: [
-      "states",
-      "effects"
-    ],
-    progress:0
-  },
-];
+
+let student = document.getElementsByTagName('select')[0].value
+
+
+// import(`./configs/student${student}.js`);
+import modules from './configs/studenta.js';
 
 // Set progress for each module (programmatically)
 const progress = Array(modules.length).fill(50); // 50% for all
@@ -108,7 +12,6 @@ const progress = Array(modules.length).fill(50); // 50% for all
 const container = document.getElementById("modules-container");
 const numModules = modules.length - 1;
 const totalProgress = modules.reduce((total, current) => total + current.progress, 0);
-const portfolioProgress = totalProgress /numModules
 
 modules.forEach((mod, idx) => {
   const card = document.createElement("div");
@@ -117,7 +20,7 @@ modules.forEach((mod, idx) => {
   card.innerHTML = `
                 <div class="progress-bar-container">
                     <div class="progress-bar" style="width: ${
-                      mod.name=="Portfolio" ? Math.max(portfolioProgress, 1) : Math.max(mod.progress, 1)
+                      Math.max(mod.progress, 1)
                     }%;"></div>
                 </div>
                 <div class="card-header">
