068be54e1d9fad9f75694da1ba7d891a7ebf3ccb
robbie <rob.643@hotmail.co.uk>
2025-08-18 19:49:44 +0100

    implement persistence for dark mode using local storage

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 let bodyElement = document.body;
 let darkModeToggle = document.getElementById("dark-mode-button");
 
+let preferences = localStorage.getItem('preferences')
+let preferencesObject;
+
+if (preferences !== null) {
+  preferencesObject = JSON.parse(preferences);
+} 
 function handleDarkMode(target) {
   if (target.id == "dark-mode-button") {
     if (target.value == 0) {
       bodyElement.classList.remove("dark-mode");
+      localStorage.setItem('preferences', JSON.stringify({darkTheme: 0}))
     } else {
       bodyElement.classList.add("dark-mode");
+      localStorage.setItem('preferences', JSON.stringify({darkTheme: 1}))
     }
   }
 }
@@ -19,4 +27,8 @@ eventTypes.forEach((eventType) => {
   });
 })
 
-handleDarkMode(darkModeToggle)
+if (preferencesObject !== null) {
+  darkModeToggle.value = Number.parseInt(preferencesObject.darkTheme);
+  handleDarkMode(darkModeToggle)
+}
+
