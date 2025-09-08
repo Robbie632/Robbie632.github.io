04bd5e4e6b2a4b86e5d2bbfd7cae1798c480a329
robbie <rob.643@hotmail.co.uk>
2025-09-08 07:57:19 +0100

    add feedback to contact form

--- main.js
+++ main.js
@@ -1,34 +1,53 @@
 let bodyElement = document.body;
 let darkModeToggle = document.getElementById("dark-mode-button");
 
-let preferences = localStorage.getItem('preferences')
+let preferences = localStorage.getItem("preferences");
 let preferencesObject = null;
 
 if (preferences !== null) {
   preferencesObject = JSON.parse(preferences);
-} 
+}
 function handleDarkMode(target) {
   if (target.id == "dark-mode-button") {
     if (target.value == 0) {
       bodyElement.classList.remove("dark-mode");
-      localStorage.setItem('preferences', JSON.stringify({darkTheme: 0}))
+      localStorage.setItem("preferences", JSON.stringify({ darkTheme: 0 }));
     } else {
       bodyElement.classList.add("dark-mode");
-      localStorage.setItem('preferences', JSON.stringify({darkTheme: 1}))
+      localStorage.setItem("preferences", JSON.stringify({ darkTheme: 1 }));
     }
   }
 }
 
-let eventTypes = ['click', 'touchend'];
+let eventTypes = ["click", "touchend"];
 
 eventTypes.forEach((eventType) => {
   document.addEventListener(eventType, (e) => {
     handleDarkMode(e.target);
   });
-})
+});
 
 if (preferencesObject !== null) {
   darkModeToggle.value = Number.parseInt(preferencesObject.darkTheme);
-  handleDarkMode(darkModeToggle)
+  handleDarkMode(darkModeToggle);
 }
 
+let contactForm = document.getElementById("contact-form");
+let contactSection = document.getElementById("contact");
+let contactFeedback = document.getElementsByClassName(
+  "contact-feedback-hidden"
+);
+
+contactForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  contactSection.style.display = "none";
+  // problem forEachnot recognise
+  contactFeedback.forEach((element) => {
+    debugger
+    element.style.classList.replace(
+      "contact-feedback-hidden",
+      "contact-feedback-show"
+    );
+  });
+});
